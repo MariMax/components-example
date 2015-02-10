ebc1d4fdf72a254564a923e77bb3ee414cee3884
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-02-10 22:45:26 +0300

    simplify code

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
                 loaders: [
                     'style',
                     'css',
-                    'autoprefixer?browsers=last 2 versions, safari 5, ie 8, ie 9, opera 12.1, ios 6, android 4',
-                    'sass?outputStyle=expanded&includePaths[]=' + (path.resolve(__dirname, './blocks'))
+                    'autoprefixer?browsers=last 2 versions',
+                    'sass'
                 ]
             },
             {
@@ -28,10 +28,10 @@ module.exports = {
             },
             { test: /\.html$/, loader: 'html-loader' },
             //load as data-urls
-            { test: /\.png$/, loader: 'url-loader?limit=10000' },
-            { test: /\.gif$/, loader: 'url-loader?limit=10000' },
-            { test: /\.woff$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-            { test: /\.svg$/, loader: 'url-loader?limit=10000' },
+            { test: /\.png$/, loader: 'url-loader?limit=20000' },
+            { test: /\.gif$/, loader: 'url-loader?limit=20000' },
+            { test: /\.woff$/, loader: 'url-loader?limit=20000&minetype=application/font-woff' },
+            { test: /\.svg$/, loader: 'url-loader?limit=20000' },
             //load as files
             { test: /\.ttf$/, loader: 'file-loader' },
             { test: /\.eot$/, loader: 'file-loader' }
